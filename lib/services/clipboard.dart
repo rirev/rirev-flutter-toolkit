3b924404348393098c1544d116597ed00daf5ac3
Mihir Sase <mihir@rirev.com>
2020-11-03 12:22:29 +0530

    Added Clipboard

---
+++ lib/services/clipboard.dart
@@ -0,0 +1,14 @@
+import 'package:clipboard/clipboard.dart';
+import 'package:meta/meta.dart';
+
+class Clipboard {
+  static final Clipboard instance = Clipboard._();
+
+  Clipboard._();
+
+  void copy({
+    @required final String content,
+  }) {
+    FlutterClipboard.copy(content);
+  }
+}
